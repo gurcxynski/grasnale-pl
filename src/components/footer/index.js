91418cd8d35c719c76f041859f69e923873f54b8
gurcxynski <wojciech.gorczynski@protonmail.com>
2026-07-13 20:39:38 +0200

    created a new post category

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,6 +5,8 @@ import style from './style';
 const Footer = () => (
 	<footer class={style.footer}>
 		<p>&copy; {new Date().getFullYear()} Grasnale. All rights reserved.</p>
+		<Link activeClassName={style.active} href="https://www.instagram.com/botc_grasnale/">Instagram</Link>
+		<Link activeClassName={style.active} href="https://www.facebook.com/profile.php?id=61569130013671">Facebook</Link>
 	</footer>
 );
 
